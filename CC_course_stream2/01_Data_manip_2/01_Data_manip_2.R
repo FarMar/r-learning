1fc09bcb0e787f77c33564365162ba0831ef0cec
Mark Farrell <chewie@marks-mbp.lan>
2020-03-25 18:39:00 +1030

    started

--- CC_course_stream2/01_Data_manip_2/01_Data_manip_2.R
+++ CC_course_stream2/01_Data_manip_2/01_Data_manip_2.R
@@ -4,3 +4,25 @@
 # Libraries
 library(dplyr)
 library(ggplot2)
+
+# Load data
+trees <- read.csv(file = "trees.csv", header = TRUE)
+head(trees)
+
+
+### Count the number of trees by species
+## stepwise first:
+# create an internal grouping structure, so that the next function acts on groups (here, species) separately. 
+trees.grouped <- group_by(trees, CommonName) 
+
+# here we use length to count the number of rows (trees) for each group (species). We could have used any row name.
+trees.summary <- summarise(trees.grouped, count = length(CommonName))
+
+## Alternatively, dplyr has a tally function that does the counts for you!
+trees.summary <- tally(trees.grouped)
+
+## Count the number of trees for each species, with a pipe!
+
+trees.summary <- trees %>% 
+  group_by(CommonName) %>% 
+  tally()
