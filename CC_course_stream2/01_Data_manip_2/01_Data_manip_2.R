c181eb253d603d31a9552d174cca3dcc8e8fe896
Mark Farrell <chewie@marks-mbp.lan>
2020-03-25 17:42:54 +1030

    setup first exercise

---
+++ CC_course_stream2/01_Data_manip_2/01_Data_manip_2.R
@@ -0,0 +1,6 @@
+# EFFICIENT DATA MANIPULATION - USE PIPES TO STREAMLINE YOUR CODE
+# Course URL: https://ourcodingclub.github.io/tutorials/data-manip-efficient/
+
+# Libraries
+library(dplyr)
+library(ggplot2)
