1206b9b1ea3783b39bf3d12883aa544b0a5838cc
Mark Farrell <chewie@marks-mbp.lan>
2020-03-27 12:15:26 +1030

    reclassifying

--- CC_course_stream2/01_Data_manip_2/01_Data_manip_2.R
+++ CC_course_stream2/01_Data_manip_2/01_Data_manip_2.R
@@ -26,3 +26,20 @@ trees.summary <- tally(trees.grouped)
 trees.summary <- trees %>% 
   group_by(CommonName) %>% 
   tally()
+
+## Summarising - quick way to generate summary dataframe for all variables
+sum.all <- summarise_all(trees, mean)
+# Not much use for this dataset as only two columns (Easting, Northing) are numeric
+
+## Reclassifying values or factors
+vector <- c(4, 13, 15, 6) # just creating a vector to evaluate
+ifelse(vector <10, "A", "B") # follows target, condition, if true, if false
+
+#The super useful case_when() is a generalisation of ifelse() that lets you assign more than two outcomes. 
+#All logical operators are available, and you assign the new value with a tilde ~. For instance:
+
+vector2 <- c("What am I?", "A", "B", "C", "D")
+case_when(vector2 == "What am I?" ~ "I am the walrus",
+          vector2 %in% c("A", "B") ~ "goo",
+          vector2 == "C" ~ "ga",
+          vector2 == "D" ~ "joob")
