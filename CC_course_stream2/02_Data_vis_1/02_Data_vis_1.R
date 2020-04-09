a23536d74bfc50a8f4c98588beb0d1cb4f27a6dc
Mark Farrell <chewie@marks-mbp.lan>
2020-04-09 18:50:03 +0930

    starting Data Vis 1

---
+++ CC_course_stream2/02_Data_vis_1/02_Data_vis_1.R
@@ -0,0 +1,11 @@
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Data Viz part 1 ######################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         09/04/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/datavis/ ######################
+
+#####################################################################################################
+
+setwd("~/DATASCHOOL/r-learning/CC_course_stream2/02_Data_vis_1/")
