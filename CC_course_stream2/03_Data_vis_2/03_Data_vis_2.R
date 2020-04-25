4e6c9c01e83c560616d5539e7646f6a8d0939dd4
Mark Farrell <chewie@marks-mbp.lan>
2020-04-25 18:14:27 +0930

    Start data vis 2

---
+++ CC_course_stream2/03_Data_vis_2/03_Data_vis_2.R
@@ -0,0 +1,27 @@
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Data Viz part 2 ######################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         25/04/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/datavis/ ######################
+
+#####################################################################################################
+
+
+### Set working directory
+setwd("~/DATASCHOOL/r-learning/CC_course_stream2/03_Data_vis_2/")
+
+
+### Install packages as needed
+
+
+
+### Load packages
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(gridExtra)
+
+## Import data
