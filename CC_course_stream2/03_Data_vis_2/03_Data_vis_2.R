d069f783bc0cab05b5625e5a66cbfb61b187abe1
Mark Farrell <chewie@marks-mbp.lan>
2020-04-25 18:49:01 +0930

    Started data vis 2

--- CC_course_stream2/03_Data_vis_2/03_Data_vis_2.R
+++ CC_course_stream2/03_Data_vis_2/03_Data_vis_2.R
@@ -18,10 +18,35 @@ setwd("~/DATASCHOOL/r-learning/CC_course_stream2/03_Data_vis_2/")
 
 
 ### Load packages
-library(tidyr)
+
 library(dplyr)
 library(ggplot2)
-library(readr)
-library(gridExtra)
+
 
 ## Import data
+magic_veg <- read.csv("magic_veg.csv")
+str(magic_veg)
+
+
+#### STARTING WITH HISTOGRAMS
+
+species_counts <- magic_veg %>% 
+  group_by(land, plot) %>% 
+  summarise(Species_number = length(unique(species)))
+
+(hist <- ggplot(species_counts, aes(x = plot)) +
+                  geom_histogram()
+  )
+
+# well, that's pretty crappy
+# This is the common way of making a histogram, when you have one observation per row and the histogram 
+# tallies them for you. But you can immediately see that it doesn’t look right, because we are working 
+# with summarised data. You therefore need to tell R that you already know how many species are in each plot. 
+# You do that by specifying the stat argument
+
+(hist <- ggplot(species_counts, aes(x = plot, y = Species_number)) +
+    geom_histogram(stat = "identity")
+)
+
+# `geom_col` would do the same thing:
+
