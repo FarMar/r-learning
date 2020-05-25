d8be20e38f45e28b86083572a5ffbb4be925d239
Mark Farrell <chewie@marks-mbp.lan>
2020-05-25 12:20:52 +0930

    started spatial data tut

---
+++ CC_course_stream2/04_Spatial_analysis/04_Spatial_analysis.R
@@ -0,0 +1,26 @@
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Spatial Data  ########################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         25/05/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/datavis/ ######################
+
+#####################################################################################################
+
+
+### Set working directory
+setwd("~/DATASCHOOL/r-learning/CC_course_stream2/04_Spatial_analysis/")
+
+
+### Install packages as needed
+
+
+
+### Load packages
+
+library(dplyr)
+library(ggplot2)
+
+
+## Import data
