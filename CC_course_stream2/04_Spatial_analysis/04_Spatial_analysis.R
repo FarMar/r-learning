e54e19b35dd44df1b5948a38bcd831199c796f25
Mark Farrell <chewie@marks-mbp.lan>
2020-05-25 15:39:42 +0930

    exploring tay raster

--- CC_course_stream2/04_Spatial_analysis/04_Spatial_analysis.R
+++ CC_course_stream2/04_Spatial_analysis/04_Spatial_analysis.R
@@ -4,7 +4,7 @@
 
 #### mark.farrell@csiro.au        +61 8 8303 8664         25/05/2020 ################################
 
-#### Their webpage is here: https://ourcodingclub.github.io/tutorials/datavis/ ######################
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/spatial/ ######################
 
 #####################################################################################################
 
@@ -14,13 +14,25 @@ setwd("~/DATASCHOOL/r-learning/CC_course_stream2/04_Spatial_analysis/")
 
 
 ### Install packages as needed
-
+install.packages(c("sp", "rgdal", "raster", "viridis", "rasterVis"))
 
 
 ### Load packages
-
-library(dplyr)
+library(sp)
+library(rgdal)
+library(raster)
 library(ggplot2)
+library(viridis)
+library(rasterVis)
 
 
 ## Import data
+tay <- raster("taycrop.tif")
+
+## Get properties for tay
+tay
+
+# We can create individual raster layers for each of the spectral bands in the raster tay.
+
+
+
