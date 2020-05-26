d3a5c6c84282224ab323110e211378cc82276911
Mark Farrell <mark.farrell@csiro.au>
2020-05-26 19:16:34 +0930

    set up manip2

---
+++ CC_course_stream3-master/01_Data_manip_2/01_Data_manip_2.R
@@ -0,0 +1,27 @@
+
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Modelling  ###########################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         26/05/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/course/mastering-modelling/index.html####
+
+#####################################################################################################
+
+
+### Set working directory
+setwd("~/DATASCHOOL/r-learning/CC_course_stream3-master/01_Data_manip_2/")
+
+
+### Install packages as needed
+
+
+
+### Load packages
+
+library(dplyr)
+library(ggplot2)
+
+
+## Import data
