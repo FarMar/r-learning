6138ae48e8a1570f86168d62c8a7530fc9eb7558
Mark Farrell <mark.farrell@csiro.au>
2020-05-27 12:44:23 +0930

    started funloops

---
+++ CC_course_stream3-master/02_Loops_and_functions/02_Loops_and_functions.R
@@ -0,0 +1,25 @@
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Modelling tutorials ##################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         27/05/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/funandloops/index.html ########
+
+#####################################################################################################
+
+### Set working directory
+
+
+
+### Install packages as needed
+
+
+
+### Load packages
+
+library(dplyr)
+library(ggplot2)
+
+
+## Import data
