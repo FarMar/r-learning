8b7e749af11845ff5faf385ba41ef7145b2324a2
Mark Farrell <mark.farrell@csiro.au>
2020-05-27 13:23:00 +0930

    data import

--- CC_course_stream3-master/02_Loops_and_functions/02_Loops_and_functions.R
+++ CC_course_stream3-master/02_Loops_and_functions/02_Loops_and_functions.R
@@ -23,3 +23,12 @@ library(ggplot2)
 
 
 ## Import data
+trees_bicuar <- read.csv("trees_bicuar.csv")
+trees_mlunguya <- read.csv("trees_mlunguya.csv")
+
+# The data contains information on tree stems surveyed in four 1 Ha plots at fieldsites around southern Africa. 
+# trees_bicuar contains data for trees in Bicuar National Park in southwest Angola, and trees_mlunguya contains 
+# data for trees in southern Mozambique. Each tree stem >5 cm trunk diameter was measured for tree height and 
+# trunk diameter, and identified to species.
+
+
