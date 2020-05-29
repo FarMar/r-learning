d4a9d27094cbc880786d08f6b3d427c134700bbb
Mark Farrell <mark.farrell@csiro.au>
2020-05-29 16:53:06 +0930

    start modelling

---
+++ CC_course_stream3-master/03_Model_design/03_Model_design.R
@@ -0,0 +1,28 @@
+
+#####################################################################################################
+
+#### Mark Farrell's trials and tribulations with Coding Club's Modelling tutorials ##################
+
+#### mark.farrell@csiro.au        +61 8 8303 8664         29/05/2020 ################################
+
+#### Their webpage is here: https://ourcodingclub.github.io/tutorials/model-design/index.html########
+
+#####################################################################################################
+
+### Set working directory
+
+
+
+### Install packages as needed
+
+
+
+### Load packages
+
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(stringr)
+
+
+## Import data
