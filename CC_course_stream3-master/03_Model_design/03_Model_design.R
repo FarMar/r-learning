e00603b03dfc1dd5409905805f9e7822a00187af
Mark Farrell <mark.farrell@csiro.au>
2020-06-02 18:19:37 +0930

    packages

--- CC_course_stream3-master/03_Model_design/03_Model_design.R
+++ CC_course_stream3-master/03_Model_design/03_Model_design.R
@@ -14,15 +14,20 @@
 
 
 ### Install packages as needed
-
+install.packages(c("lme4", "sjPlot", "ggeffects", "MCMCglmm", "MCMCvis", "brms", "stargazer"))
 
 
 ### Load packages
 
 library(dplyr)
 library(ggplot2)
-library(gridExtra)
-library(stringr)
+library(lme4)
+library(sjPlot)
+library(ggeffects)
+library(MCMCglmm)
+library(MCMCvis)
+library(brms)
+library(stargazer)
 
 
 ## Import data
